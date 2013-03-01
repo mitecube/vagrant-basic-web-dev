a64dafd4ad7af56e9e4aea4e5c008f9e4bdb41b3
Fabio Dellutri <fabio.dellutri@mitecube.com>
2013-03-01 01:24:37 +0100

    Changes to README and init.pp

--- README.md
+++ README.md
@@ -5,3 +5,38 @@ Description
 -----------
 
 Setup a dev environment for Mitecube.
+
+This vagrant configuration sets up a basic LAMP environment suited for Symfony 2 development:
+
+* Based on Ubuntu Lucid
+* Apache 2.2
+* PHP 5.3 with intl and readline, xdebug, sqlite extensions
+* phpMyAdmin
+* MySQL Server 5.1
+* Git and SVN clients
+
+# Prerequisites
+
+## Install Vagrant
+
+Obviously, you need [Vagrant](http://www.vagrantup.com/), which in turn requires Ruby and VirtualBox. Vagrant runs on Linux, OS X, and Windows, although some special configuration applies to Windows (see below).
+
+## Download and install a base image
+
+	$ vagrant box add vagrant-basic-web-dev http://files.vagrantup.com/precise64.box
+
+This example uses the default Ubuntu image from the Vagrant project, although you can use other Ubuntu boxes if you like. If you do not name the box "base", you will later on need to adapt the Vagrantfile in the project root directory.
+
+## Setup a working directory and start your new environment
+
+    $ git clone https://github.com/mitecube/vagrant-basic-web-dev.git mydir
+    $ cd mydir
+    $ vagrant up
+
+Depending on the versions of the box and your VirtualBox installation, you might see a notice that the guest additions of the box do not match the version of VirtualBox you are using. If you encounter any problems, you might want to install up to date guest additions on your box once running and [repackage it for use with Vagrant](http://vagrantup.com/docs/getting-started/packaging.html).
+
+If you prefer a clean URL, you might want to map `33.33.33.100` to a local domain of your choice in your hosts file. This is entirely optional.
+
+## Use it
+
+TODO	
--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,7 @@ class development {
 
 }
 
-class datacube_php {
+class php_dev {
 
     php::module { [
         'curl', 'gd', 'mcrypt', 'memcached', 'mysql',
@@ -74,32 +74,21 @@ class datacube_php {
         notify => Service['apache'],
     }    
     
-#  	file { "/etc/mysql/my.cnf":
-#      mode => 644, owner => root, group => root,
-#      source  => '/vagrant/conf/mysql/my.cnf'
-#  }    
-
 }
 
 class { "mysql":
   root_password => 'root',          
   source  => '/vagrant/conf/mysql/my.cnf'
 }
-mysql::grant { 'datacube':
-  mysql_privileges => 'ALL',
-  mysql_password => 'datacube',
-  mysql_db => 'datacube',
-  mysql_user => 'datacube',
-  mysql_host => '%',
-}
 
-user { "datacube":
-	ensure => "present",
-}
-file { '/datacube':
-   ensure => 'link',
-   target => '/vagrant/project',
-}
+#mysql::grant { 'dbname':
+#  mysql_privileges => 'ALL',
+#  mysql_db => 'dbname',
+#  mysql_user => 'dbuser',
+#  mysql_password => 'dbpass',
+#  mysql_host => '%',
+#}
+
 
 file { "/var/log/mongo":
     ensure => "directory",
@@ -108,70 +97,12 @@ file { "/var/lib/mongo":
     ensure => "directory",
 }
 
-class { "redis": 
-	source  => '/vagrant/conf/redis/redis.conf'
-}
+#class { "redis": 
+#	source  => '/vagrant/conf/redis/redis.conf'
+#}
 
-class { 'gearman': }
+#class { 'gearman': }
 
-supervisor::service {
-  'MitecubeDatacubeBundleWorkerCrawlerManagerWorker':
-    ensure      => present,
-    enable      => true,
-    command     => '/usr/bin/php /vagrant/project/app/console gearman:worker:execute MitecubeDatacubeBundleWorkerCrawlerManagerWorker --no-interaction',
-    environment => '',
-    numprocs 	=> '2',
-    autorestart	=> true,
-    user        => 'datacube',
-    group       => 'datacube',
-    require     => [ Package['gearman-job-server'], User['datacube'] ];
-
-  'MitecubeDatacubeBundleWorkerCrawlerDependentWorker':
-    ensure      => present,
-    enable      => true,
-    command     => '/usr/bin/php /vagrant/project/app/console gearman:worker:execute MitecubeDatacubeBundleWorkerCrawlerDependentWorker --no-interaction',
-    environment => '',
-    numprocs 	=> '10',
-    autorestart	=> true,    
-    user        => 'datacube',
-    group       => 'datacube',
-    require     => [ Package['gearman-job-server'], User['datacube'] ];
-    
-
-  'MitecubeDatacubeBundleWorkerManagerWorker':
-    ensure      => present,
-    enable      => true,
-    command     => '/usr/bin/php /vagrant/project/app/console gearman:worker:execute MitecubeDatacubeBundleWorkerManagerWorker --no-interaction',
-    environment => '',
-    numprocs 	=> '2',
-    autorestart	=> true,
-    user        => 'datacube',
-    group       => 'datacube',
-    require     => [ Package['gearman-job-server'], User['datacube'] ];
-
-  'MitecubeDatacubeBundleWorkerEtlCustomCodeWorker':
-    ensure      => present,
-    enable      => true,
-    command     => '/usr/bin/php /vagrant/project/app/console gearman:worker:execute MitecubeDatacubeBundleWorkerEtlCustomCodeWorker --no-interaction',
-    environment => '',
-    numprocs 	=> '10',
-    autorestart	=> true,    
-    user        => 'datacube',
-    group       => 'datacube',
-    require     => [ Package['gearman-job-server'], User['datacube'] ];
-    
-
-  'MitecubeQueueBundleWorkerTestWorker':
-    ensure      => present,
-    enable      => true,
-    command     => '/usr/bin/php /vagrant/project/app/console gearman:worker:execute MitecubeQueueBundleWorkerTestWorker --no-interaction',
-    environment => '',
-    numprocs 	=> '1',
-    autorestart	=> true,    
-    user        => 'datacube',
-    group       => 'datacube',
-    require     => [ Package['gearman-job-server'], User['datacube'] ];    
-}
 
 class { 'elasticsearch':
   version      => '0.20.4',
@@ -192,7 +123,7 @@ include php::apache2
 include apache
 include redis
 
-include datacube_php
+include php_dev
 include development
 
 include mysql
@@ -201,6 +132,6 @@ class {'mongodb':
   enable_10gen => true,
 }
 
-include supervisor
+#include supervisor
 
 #include phpqatools
