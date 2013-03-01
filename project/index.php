1682b66d47c850145ad7d6441f00f93840672c9f
Fabio Dellutri <fabio.dellutri@mitecube.com>
2013-03-01 01:38:48 +0100

    Removed elasticsearch submodule

--- project/index.php
+++ project/index.php
@@ -1,8 +1,2 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: fdellutri
- * Date: 01/03/13
- * Time: 01:30
- * To change this template use File | Settings | File Templates.
- */
+phpinfo();
